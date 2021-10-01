4fcd6df7ee990fb78a8b5d7c16c2a5b27337a02b
Artem Eroshenko <eroshenkoam@qameta.io>
2021-10-01 14:13:51 +0300

    Initial commit

---
+++ src/test/java/io/github/itishniki/actions/SimpleTest.java
@@ -0,0 +1,14 @@
+package io.github.itishniki.actions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SimpleTest {
+
+    @Test
+    public void testSomething() {
+        assertThat(1).isEqualTo(1);
+    }
+
+}
